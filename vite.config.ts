d85a3668a89183c5671665e5eec91e7583c96721
HANTECK <ianroystudies@gmail.com>
2026-01-16 02:10:03 +0800

    added some new files like index

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,6 @@ export default defineConfig({
       output: {
         manualChunks: {
           vendor: ["react", "react-dom", "react-icons"],
-          motion: ["framer-motion"],
         },
       },
     },
